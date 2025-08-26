79825036bc3cc88a18943e6d1aa35650e2c656d1
JPPVC <joavic@alunos.utfpr.edu.br>
2025-08-26 14:27:10 -0300

    added artigos

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
 	$('#blog-feed-b').click(function() {
-    		
+    		$('#feed').attr('src', 'blogfeed.html')
 	});
 	
 	$('#about-me-b').click(function() {
-
+		console.log('clicked')
+		$('#feed').attr('src', 'aboutmefeed.html')
 	});
+
+	$('#artigos-b').click(function(){
+		$('#feed').attr('src', 'artigosfeed.html')
+	});
+	
+	$('#projects-b'.click(function() {
+		$('#feed').attr('src', 'projectsfeed.html')
+	});
+	
+	$('#link-b'.click(function() {
+		$('#feed').attr('src', 'linkfeed.html')
+	});
+	
+	$('#about-page-b'.click(function() {
+		$('#feed').attr('src', 'aboutmefeed.html')
+	});
+	
+	$('#mailme').click(function() {
+		$('#mail-div').attr('src', 'mail.html')
+	})
 });
